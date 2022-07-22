9e0a77371a76b0348a82cabf2fd40ef0c4a4318f
Franziska Ahrend <franziska@ahrend.de>
2022-07-22 03:37:57 +0200

    Complete Code in one Shell file
    Code can be executed with
    bash FAStage0.sh

---
+++ FAStage0.sh
@@ -0,0 +1,63 @@
+#BioHack - Stage 0 - Submission by Franziska Ahrend
+
+#to run this through the terminal: bash FAStage0.sh
+
+firstname="Franziska"
+lastname="Ahrend"
+echo $firstname $lastname 
+#Output: Franziska Ahrend
+
+firstname="Franziska"
+lastname="Ahrend"
+echo $firstname 
+echo $lastname
+#Franziska
+#Ahrend
+
+
+
+#Notebook 1
+echo Notebook1
+mkdir Franziska
+mkdir biocomputing && cd biocomputing
+wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk 
+mv wildtype.fna ../Franziska/
+rm -r wildtype.gbk.1
+cd ../Franziska && grep -c  "tatatata" wildtype.fna 
+grep "tatatata" wildtype.fna > mutant
+clear; history
+ls && ls ../biocomputing 
+#exit
+cd ..
+
+#Notebook 2
+echo Notebook2
+#0.2.1
+figlet Franziska 
+#0.2.3
+mkdir compare
+#0.2.3a
+cd compare && wget https://www.bioinformatics.babraham.ac.uk/training/Introduction%20to%20Unix/unix_intro_data.tar.gz 
+#0.2.3b
+gunzip unix_intro_data.tar.gz
+#0.2.3c
+tar -xvf unix_intro_data.tar
+#0.2.3d
+cd seqmonk_genomes/Saccharomyces\ cerevisiae/EF4 && grep rRNA Mito.dat 
+#0.2.3e
+cp Mito.dat ../../../
+cd ..; cd ..; cd ..
+#0.2.3fi
+nano Mito.dat
+#now change  Mito to Mitochondrion in the ID and AC header lines
+#save the file and exit nano: ctrl+x	y	Return
+#0.2.3fiii
+mv Mito.dat Mitochondrion.txt
+#0.2.4
+cd FastQ_Data
+#0.2.4a
+wc -l < lane8_DD_P4_TTAGGC_L008_R1.fastq.gz
+#0.2.4b
+wc -l *.fastq.gz | tail -1 && wc -l *.fastq.gz | tail -1 > lineCounter.txt 
--- Stage0/firstlastname2lines.bash
+++
@@ -1,9 +0,0 @@
-#BioHack - Submission by Franziska Ahrend
-#Task:
-#Write a simple Bash program where your first name and last name are assigned to different variables, 
-#and the script prints out your full name.
-
-firstname="Franziska"
-lastname="Ahrend"
-echo $firstname 
-echo $lastname
