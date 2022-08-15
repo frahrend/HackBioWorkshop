c51c4592582674beba1c0554ade144e0282a469a
Franziska Ahrend <franziska@ahrend.de>
2022-08-15 18:49:40 -0400

    Stage 3 first part
    Step 4/8

---
+++ Stage 3/FAStage3.R
@@ -0,0 +1,38 @@
+# Submission Franziska Ahrend - Stage 3 - Hack Bio
+print('Submission by Franziska Ahrend')
+
+
+# Install DeSeq2 via bioconductor on your RStudio
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("DESeq2")
+
+
+# Download the dataset from gene expression omnibus
+# Series GSE129024 from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE129024
+print('Dataset downloaded from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE129024')
+
+# Import it into Rstudio
+
+
+
+# Perform differential expression analysis
+
+
+
+
+# Visualize the output as a volcano plot and highlight the upregulated and downregulated datasets
+
+
+
+
+
+# Detect the cell types.
+
+
+
+
+
+
+
